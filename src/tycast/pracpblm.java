cc906ba05bc73eaa969f38e2744f808f8379c38e
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-04-07 20:55:32 +0530

    typecast
    typecasted and converted short to foat

---
+++ src/tycast/pracpblm.java
@@ -0,0 +1,21 @@
+package tycast;
+
+public class pracpblm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		short a=3274;
+		int b=365;
+		System.out.println(a);
+		System.out.println(b);
+		b=a;
+		float f=3.564f;
+		f=b;
+		System.out.println(f);
+		
+		
+
+	}
+
+}
